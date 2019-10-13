a79f41413aa17ee478df6f532a2e1f85bd460c40
David Coffey <dcoffey@fredhutch.org>
2019-10-12 23:06:40 -0700

    Initial commit

---
+++ R/TCellPack.R
@@ -0,0 +1,168 @@
+#' Create T cell pack
+#'
+#' Creates a T cell pack from a gliph file with the option to visualize clonotype and cellular metadata.
+#'
+#' @param gliph Character vector providing the path to GLIPH_TCR_Table-convergence-groups.txt.
+#' @param clonotype.data Optional 2 column data frame named "clonotype", "frequency".
+#' @param cell.data Optional 3 column data frame named "clonotype", "cell", and "data".
+#' @param specificity.color Character vector providing the color (single value) of the specificity circles.
+#' @param clonotype.color Character vector providing the color (single value) of the clonotype circles.
+#' @param cell.color Character vector providing the color (single value)  of the cell circles.
+#' @param color.gradient Character vector providing the RColorBrewer color palette used color circles with discrete or continuous values.
+#' @param line.color Character vector providing the color (single value) of the circle borders.
+#' @param label.color Character vector providing the color (single value) of the labels.
+#' @param label.size Integer corresponding to text size of the labels.
+#' @param label Character vector indicating if labels should be displayed.  Options include "none", "specificity", "clonotype", and "cell".
+#' @param legend Logic indicating if legend should be displayed (TRUE) or not (FALSE).
+#' @param cluster.size Minimum number of clonotypes per specificity group.
+#'
+#' @return Returns a T cell circle packing plot.
+#'
+#' @examples
+#'
+#' @export
+#' @import ggplot2
+#' @import ggraph
+#' @import igraph
+#' @importFrom plyr ldply
+#' @importFrom stringr str_split
+#' @importFrom data.table fread
+
+TCellPack <- function(gliph,
+                      clonotype.data = NULL,
+                      cell.data = NULL,
+                      specificity.color = "#08306b",
+                      clonotype.color = "#4292c6",
+                      cell.color = "#9ecae1",
+                      color.gradient = "RdYlBu",
+                      line.color = "black",
+                      label.color = "black",
+                      label.size = 1,
+                      label = "none",
+                      legend = FALSE,
+                      cluster.size = 1){
+
+  # Input validation
+  if(!is.null(cell.data)){
+    if(!(all(names(cell.data) %in% c("clonotype", "cell", "data")))) {warning("cell.data must have the columns 'clonotype', 'cell', and 'data", call. = FALSE)}
+    if(any(is.na(cell.data$data))) {warning("cell.data cannot contain NA", call. = FALSE)}
+  }
+
+  if(!is.null(clonotype.data)){
+    if(!(all(names(clonotype.data) %in% c("clonotype", "frequency")))) {warning("clonotype.data must have the columns 'clonotype' and 'frequency'", call. = FALSE)}
+    if(any(clonotype.data$frequency %in% 0 & is.na(clonotype.data$frequency))) {warning("clonotype.data cannot contain NA or 0 values", call. = FALSE)}
+  }
+
+  if(class(gliph) == "character"){
+    gliph <- data.frame(fread(gliph))
+  }
+  gliph <- data.frame(gliph)
+
+  # Reformat GLIPH data
+  specifities <- stringr::str_split(string = gliph[gliph[,1] > cluster.size, 3], pattern = " ")
+  names(specifities) <- 1:length(specifities)
+  specifities <- plyr::ldply(specifities, data.frame)
+  names(specifities) <- c("specificity", "clonotype")
+
+  # Gliph data only
+  if(is.null(clonotype.data) & is.null(cell.data)){
+    edges <- data.frame(from = specifities$specificity, to = specifities$clonotype)
+
+    # Graph object
+    graph <- igraph::graph_from_data_frame(d = edges)
+
+    # Plot
+    plot <- ggraph::ggraph(graph, layout = "circlepack") +
+      geom_node_circle(aes(fill = factor(depth)), size = 0.25, n = 50, color = line.color) +
+      scale_fill_manual(values = c("0" = specificity.color, "1" = clonotype.color),
+                        name = "", labels = c("Specificity", "Clonotype")) +
+      theme_void() +
+      coord_fixed()
+  }
+
+  # Clonotype data only
+  if(!is.null(clonotype.data) & is.null(cell.data)){
+    specifities <- merge(specifities, clonotype.data, all = FALSE)
+    specifities.aggregate <- aggregate(data = specifities, frequency~specificity, sum)
+    names(specifities.aggregate) <- c("name", "frequency")
+    names(clonotype.data) <- c("name", "frequency")
+    vertices <- rbind(clonotype.data, specifities.aggregate)
+    edges <- data.frame(from = specifities$specificity, to = specifities$clonotype)
+
+    # Graph object
+    graph <- igraph::graph_from_data_frame(d = edges, vertices = vertices)
+
+    plot <- ggraph::ggraph(graph, layout = "circlepack", weight = frequency) +
+      geom_node_circle(aes(fill = factor(depth)), size = 0.25, n = 50, color = line.color) +
+      scale_fill_manual(values = c("0" = specificity.color, "1" = clonotype.color),
+                        name = "", labels = c("Specificity", "Clonotype")) +
+      theme_void() +
+      coord_fixed()
+  }
+
+  # Continuous cell data only
+  if(!is.null(cell.data) & class(cell.data$data) %in% c("integer", "numeric")){
+    specifities <- merge(specifities, cell.data, all = FALSE)
+    edges <- data.frame(from = specifities$specificity, to = specifities$clonotype)
+    edges <- rbind(edges, data.frame(from = cell.data$clonotype, to = cell.data$cell))
+    specifities.aggregate <- aggregate(data = specifities, data~specificity, mean)
+    names(specifities.aggregate) <- c("name", "data")
+    clonotype.aggregate <- aggregate(data = specifities, data~clonotype, mean)
+    names(clonotype.aggregate) <- c("name", "data")
+    vertices <- do.call("rbind", list(data.frame(name = cell.data$cell, data = cell.data$data),
+                                     specifities.aggregate, clonotype.aggregate))
+    # Graph object
+    graph <- igraph::graph_from_data_frame(d = edges, vertices = vertices)
+
+    # Plot
+    plot <- ggraph::ggraph(graph, layout = "circlepack") +
+      geom_node_circle(aes(fill = data), size = 0.25, n = 50, color = line.color) +
+      scale_fill_distiller(palette = color.gradient) +
+      theme_void() +
+      coord_fixed() +
+      labs(fill = "")
+  }
+
+  # Discrete cell data only
+  if(!is.null(cell.data) & class(cell.data$data) %in% c("factor", "character")){
+    specifities <- specifities[specifities$clonotype %in% cell.data$clonotype,]
+    edges <- data.frame(from = specifities$specificity, to = specifities$clonotype)
+    edges <- rbind(edges, data.frame(from = cell.data$clonotype, to = cell.data$cell))
+    vertices <- data.frame(name = cell.data$cell, data = cell.data$data)
+    vertices <- rbind(vertices, unique(data.frame(name = specifities$specificity, data = rep("1", nrow(specifities)))))
+    vertices <- rbind(vertices, unique(data.frame(name = specifities$clonotype, data = rep("2", nrow(specifities)))))
+
+    # Graph object
+    graph <- igraph::graph_from_data_frame(d = edges, vertices = vertices)
+
+    # Plot
+    getPalette <- grDevices::colorRampPalette(RColorBrewer::brewer.pal(9, color.gradient))
+    plot <- ggraph::ggraph(graph, layout = "circlepack") +
+      geom_node_circle(aes(fill = data), size = 0.25, n = 50, color = line.color) +
+      scale_fill_manual(values = c(specificity.color, clonotype.color, getPalette(length(unique(cell.data$data)))), breaks = unique(cell.data$data)) +
+      theme_void() +
+      coord_fixed() +
+      labs(fill = "")
+  }
+
+  if(legend == FALSE){
+    plot <- plot + theme(legend.position="none")
+  }
+
+  if(label == "cell"){
+    plot + geom_node_text(aes(label=name, filter = depth == 2), color = label.color, size = label.size)
+  }
+
+  if(label == "clonotype"){
+    plot + geom_node_text(aes(label=name, filter = depth == 1), color = label.color, size = label.size)
+  }
+
+  if(label == "specifity"){
+    plot + geom_node_text(aes(label=name, filter = depth == 0), color = label.color, size = label.size)
+  }
+
+  return(plot)
+}
+
+
+
---
+++ README.md
@@ -0,0 +1,10 @@
+# TCellPack v0.1
+TCellPack is a package used to visualize characteristics of T cell repertoires.  It is possible to visualize T cell clonotype diversity and abundance in relation to its antigen specificity and phenotype in a single plot.
+
+### Installation instructions
+```
+install.packages("devtools")
+devtools::install_github("davidcoffey/TCellPack")
+```
+### Tutorial
+Coming soon.
